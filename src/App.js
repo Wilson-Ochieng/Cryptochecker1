46943ec0363bbbc0ce902c37efcb88aa9e36a533
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-07 20:37:33 +0300

    imported axios dependencies,useState and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App({handleChange}) {
+function App({ handleChange }) {
   return (
     <div className="App">
       <div className="coin-app">
-          <div className="coin-search">
-        <h1 className="coin-text">CryptoChecker</h1>
-        <form>
-          <input
-            type="text"
-            placeholder="Search"
-            className="coin-input"
-              onChange={handleChange} />
-        </form>
+        <div className="coin-search">
+          <h1 className="coin-text">CryptoChecker</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Search"
+              className="coin-input"
+              onChange={handleChange}
+            />
+          </form>
         </div>
-        </div>
-     
+      </div>
     </div>
   );
 }
