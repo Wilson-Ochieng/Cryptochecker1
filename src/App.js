1e74ae69e83791727560341f1f4b0c08359a6e21
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-07 20:41:17 +0300

    Passed an empty array to coin ,and empty  string to  search

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App({ handleChange }) {
+
+
+
+  function App({ }) {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  };
   return (
     <div className="App">
       <div className="coin-app">
