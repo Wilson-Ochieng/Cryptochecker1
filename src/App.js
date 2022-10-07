cda3e21ff99b4df5e8a8f9bc30c46f299ded6a2d
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-07 20:46:46 +0300

    Mapped the coins

--- src/App.js
+++ src/App.js
@@ -43,6 +43,20 @@ function App({ handleChange }) {
             />
           </form>
         </div>
+         {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
       </div>
     </div>
   );
