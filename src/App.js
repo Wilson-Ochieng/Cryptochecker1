13c679f6476caa5704b4ef825f28608a471a82c3
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-07 20:25:00 +0300

    created Search and coin-input  divs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <div className="coin-app">
           <div className="coin-search">
-        
+        <h1 className="coin-text">CryptoChecker</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}/>
      
     </div>
   );
